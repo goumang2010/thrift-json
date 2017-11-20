5f7ea445468bd4a5b589f082dbcdf7bcb1cdabda
goumang2010 <goumang2010@live.com>
2017-11-20 16:52:43 +0800

    js launch function

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+var PythonShell = require('python-shell');
+var path = require('path');
+module.exports = exports.default = function (files) {
+    if (files && (typeof files === 'string')) files = [files];
+    else if (!Array.isArray(files)) return Promise.reject('Please input files array');
+    return new Promise(function (resolve, reject) {
+        var thriftpy = new PythonShell('thrift_to_json.py');
+        var result = [];
+        var idx = 0;
+        // sends a message to the Python script via stdin
+        thriftpy.on('message', function (message) {
+            if (message)
+                result.push({ source: files[idx], value: JSON.parse(message) });
+            idx++;
+        });
+        files.forEach(function (x) {
+            thriftpy.send(path.resolve(process.cwd(), x));
+        });
+        thriftpy.end(function (err) {
+            if (err)
+                reject(err);
+            resolve(result);
+        });
+    });
+};
