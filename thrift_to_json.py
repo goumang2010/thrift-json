421b01b83100858d63a966a9417edf48318c3714
goumang2010 <goumang2010@live.com>
2017-11-20 15:33:15 +0800

    move main file

--- thrift_to_json.py
+++ thrift_to_json.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 # !/usr/bin/env python
 import sys, getopt
-from parser import parse
-from Type import DataType
+from thriftpy.parser import parse
+from thriftpy.Type import DataType
 
 """
     translate .thrift to json
